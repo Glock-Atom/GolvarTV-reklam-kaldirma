8159b3d11d64c41c03624f7b579c5cdce5caa38c
Glock A. <hamzatek36@gmail.com>
2025-02-11 22:10:36 +0100

    initial commit

---
+++ Golvar-TV-Reklam-kaldirma-2025-02-11.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Golvar-TV-Reklam-kaldirma
+// @namespace    http://tampermonkey.net/
+// @version      2025-02-11
+// @description  Golvar TV altta cikan reklami kaldirma scripti
+// @author       Glock A.
+// @include      https://golvar*.*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=golvar3194.sbs
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.querySelectorAll('img[src="https://golvar3194.sbs/wp-content/uploads/2023/09/6kv47c6.gif"]').forEach(img => img.remove());
+    document.querySelectorAll(".stream-ads").forEach(el => {
+        el.style.visibility = "hidden";
+    });
+
+
+    let observer = new MutationObserver(() => {
+        document.querySelectorAll('img[src="https://golvar3194.sbs/wp-content/uploads/2023/09/6kv47c6.gif"]').forEach(img => img.remove());
+        document.querySelectorAll(".stream-ads").forEach(el => {
+            el.style.visibility = "hidden";
+        });
+    });
+
+    console.log("Golvar TV Reklam kaldirma: Reklam kaldirildi!");
+    observer.observe(document.body, { childList: true, subtree: true });
+
+
+})();
