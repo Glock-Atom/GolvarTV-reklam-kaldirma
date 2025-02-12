89e365b80c60cb841b031c5a6ad5bc28abf9b1c7
Glock A. <hamzatek36@gmail.com>
2025-02-12 17:57:11 +0100

    ...

--- Golvar-TV-Reklam-kaldirma-2025-02-11.user.js
+++ Golvar-TV-Reklam-kaldirma-2025-02-11.user.js
@@ -17,6 +17,7 @@
         el.style.visibility = "hidden";
     });
 
+    
 
     let observer = new MutationObserver(() => {
         document.querySelectorAll('img[src="https://golvar3194.sbs/wp-content/uploads/2023/09/6kv47c6.gif"]').forEach(img => img.remove());
@@ -28,5 +29,14 @@
     console.log("Golvar TV Reklam kaldirma: Reklam kaldirildi!");
     observer.observe(document.body, { childList: true, subtree: true });
 
+    function checkUpdate(){
+        if(window.top !== window.self && !(window.location.href.includes("golvar[0-9].[a-z]")));
+            return;
+    }
+
+    const scriptURL = "https://github.com/Glock-Atom/GolvarTV-reklam-kaldirma/blob/main/Golvar-TV-Reklam-kaldirma-2025-02-11.user.js";
+
+    fetch(scriptURL)
+        .then(response => )
 
 })();
